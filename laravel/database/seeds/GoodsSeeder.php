ceb9551cdd4aee06ee23de6ffc3317bec4604f95
koms <komsldt@gmail.com>
2020-08-02 18:18:03 +0300

    Goods model and db seeder

---
+++ laravel/database/seeds/GoodsSeeder.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GoodsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        \App\Goods::truncate();
+
+        DB::table('goods')->insert([
+            'name' => 'cola',
+            'description' => '2l',
+            'image' => 'cola.jpg',
+            'type' => 'beverages',
+            'price_usd' => rand(500, 1500) / 100.0,
+            'price_euro' => rand(500, 1500) / 100.0,
+        ]);
+
+        $pizzas = [
+            'Margherita' => 'Cheese and tomato.',
+            'Hawaiian' => 'Ham and pineapple.',
+            'Classic' => 'Mushroom, bacon, and fresh tomato.',
+            'American Hot' => 'Pepperoni, green pepper, onion, and chillies.',
+            'BBQ Feast' => 'Onions, green peppers, bacon, spicy beef, and bbq sauce.',
+            'BBQ Chicken' => 'Plain chicken, bbq sauce, onions, and green peppers.',
+            'Chicken Supreme' => 'Chicken tikka, mushroom, green peppers, and sweetcorn.',
+            'Vegetarian' => 'Mushrooms, green peppers, onions, and sweetcorn.',
+            'Vegetarian Hot' => 'Mushrooms, green peppers, onions, and jalapeno.',
+            'Meat Lovers' => 'Ham, beef, pepperoni, and bacon.'
+        ];
+
+        foreach ($pizzas as $name => $desc) {
+            DB::table('goods')->insert([
+                'name' => $name,
+                'description' => $desc,
+                'image' => 'pizza.jpg',
+                'type' => 'pizza',
+                'price_usd' => rand(500, 1500) / 100.0,
+                'price_euro' => rand(500, 1500) / 100.0,
+            ]);
+        }
+
+        DB::table('goods')->insert([
+            'name' => 'juice',
+            'description' => '1l',
+            'image' => 'juice.jpg',
+            'type' => 'beverages',
+            'price_usd' => rand(500, 1500) / 100.0,
+            'price_euro' => rand(500, 1500) / 100.0,
+        ]);
+
+        DB::table('goods')->insert([
+            'name' => 'Caesar salad',
+            'description' => '200g',
+            'image' => 'caeser.jpg',
+            'type' => 'salads',
+            'price_usd' => rand(500, 1500) / 100.0,
+            'price_euro' => rand(500, 1500) / 100.0,
+        ]);
+
+    }
+}
