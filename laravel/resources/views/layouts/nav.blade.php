eecb2d9a28aefdf659f3d297aea70fe0f53a59f7
koms <komsldt@gmail.com>
2020-08-05 10:38:59 +0300

    added icons removed "Cart" from nav

--- laravel/resources/views/layouts/nav.blade.php
+++ laravel/resources/views/layouts/nav.blade.php
@@ -7,7 +7,6 @@
         <div class="cartButton">
             <img src="/icons/cart.svg">
             <span class=" @unless(isset($cartQuantity) && $cartQuantity !== 0) d-none @endif cartQuantity">{{$cartQuantity??''}}</span>
-            Cart
             <div class="cart--details column">
                 @if(isset($cartQuantity) && $cartQuantity !== 0)
                     @include('partial.cartDetails')
